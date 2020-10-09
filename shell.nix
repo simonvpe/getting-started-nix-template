dccdf6e112ff0f7d9fec81418612f2f560063f92
Simon Pettersson <simon.v.pettersson@gmail.com>
2020-10-09 21:54:43 +0200

    Fixes nix-shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 { project ? import ./nix {} }:
 
 (project.pixie.overrideAttrs (pixie: {
-  nativeBuildInputs = pixie.nativeBuildInputs ++ project.devTools;
-  shellHook = pixie.shellHook + ''
-    ${project.ci.pre-commit-check}
+  nativeBuildInputs = (pixie.nativeBuildInputs or []) ++ project.devTools;
+  shellHook = (pixie.shellHook or "") + ''
+    echo HELO
+    ${project.ci.pre-commit-check.shellHook}
   '';
 })).env
 
